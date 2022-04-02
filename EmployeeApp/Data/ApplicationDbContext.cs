b91e2b69288e0708245a435c6093d4e9a60c355e
mtrzyna <mateusz.trzyna@nexio.pl>
2022-04-02 22:52:49 +0200

    Add project files.

---
+++ EmployeeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Employee>().ToTable("Employees", "HR");
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeApp/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeApp.Models
+{
+    [Table("Departments", Schema = "HR")]
+    public class Department
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [Display(Name = "Department Name")]
+        [Column(TypeName = "varchar(200)")]
+        public string DepartmentName { get; set; } = string.Empty;
+    }
+}
